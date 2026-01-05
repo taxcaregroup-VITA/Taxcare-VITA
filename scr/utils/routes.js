03c35fa3f4b38f695b2940c4be24ac603bb3ade9
taxcaregroup-VITA <taxcaregroupinc24@gmail.com>
2026-01-05 16:56:30 -0500

    Add routes.js (site routing)

---
+++ scr/utils/routes.js
@@ -0,0 +1,11 @@
+// src/utils/routes.js
+export const routes = {
+  Home: "/",
+  GetStarted: "/get-started",
+  Schedule: "/schedule",
+  FAQ: "/faq",
+  About: "/about",
+  Privacy: "/privacy",
+  Contact: "/contact",
+  Dashboard: "/dashboard"
+};
