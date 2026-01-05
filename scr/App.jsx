0ba053d9388e9c9aedb1da6842264d4670c3386b
taxcaregroup-VITA <taxcaregroupinc24@gmail.com>
2026-01-05 18:57:14 -0500

    Update App.jsx

--- scr/App.jsx
+++ scr/App.jsx
@@ -1,11 +1,10 @@
+// src/App.jsx
 import React from 'react';
 
 export default function App() {
   return (
-    <div className="text-center mt-20">
-      <h1 className="text-3xl font-bold text-emerald-600">
-        Tax Care VITA is live!
-      </h1>
+    <div className="text-center mt-20 text-3xl text-emerald-600">
+      Tax Care VITA is live!
     </div>
   );
 }
