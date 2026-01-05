49561fa189d9a2ae0cb7881bb14949c00d960fce
taxcaregroup-VITA <taxcaregroupinc24@gmail.com>
2026-01-05 18:41:32 -0500

    Update Home.jsx

--- scr/pages/Home.jsx
+++ scr/pages/Home.jsx
@@ -1,23 +1,18 @@
 import React from 'react';
-import { useLanguage } from '../layout';
-import { useTranslation } from '@/components/common/translations';
-import HeroSection from '@/components/home/HeroSection';
-import FeaturesSection from '@/components/home/FeaturesSection';
-import EligibilitySection from '@/components/home/EligibilitySection';
-import TestimonialsSection from '@/components/home/TestimonialsSection';
-import SignUpSection from '@/components/home/SignUpSection';
+import HeroSection from '../components/HeroSection';
+import FeaturesSection from '../components/FeaturesSection';
+import EligibilitySection from '../components/EligibilitySection';
+import TestimonialsSection from '../components/TestimonialsSection';
+import SignUpSection from '../components/SignUpSection';
 
 export default function Home() {
-  const { language } = useLanguage();
-  const t = useTranslation(language);
-
   return (
-    <div>
-      <HeroSection t={t} />
-      <FeaturesSection t={t} />
-      <EligibilitySection t={t} />
-      <TestimonialsSection t={t} />
-      <SignUpSection t={t} language={language} />
-    </div>
+    <>
+      <HeroSection />
+      <FeaturesSection />
+      <EligibilitySection />
+      <TestimonialsSection />
+      <SignUpSection />
+    </>
   );
 }
