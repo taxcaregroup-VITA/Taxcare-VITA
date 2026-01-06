fb83566f02c2d565fd619481906f7bcaf838cded
taxcaregroup-VITA <taxcaregroupinc24@gmail.com>
2026-01-06 18:34:37 -0500

    Create Layout.js

---
+++ src/Layout.js
@@ -0,0 +1,9 @@
+const navItems = [
+  { name: t('home'), page: 'Home' },
+  { name: t('getStarted'), page: 'GetStarted' },
+  { name: t('scheduleAppointment'), page: 'Schedule' },
+  { name: t('faq'), page: 'FAQ' },
+  { name: t('aboutUs'), page: 'About' },
+  { name: t('privacyPolicy'), page: 'Privacy' },
+  { name: t('contact'), page: 'Contact' },
+];
