ac7ceec0f7234a7ea41b3d62cd9ed965f03cf8a4
taxcaregroup-VITA <taxcaregroupinc24@gmail.com>
2026-01-06 18:31:32 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Layout from './Layout'
 import Home from './Home'
-// Example: other pages you might add
-// import About from './About'
-// import FAQ from './FAQ'
-// import Contact from './Contact'
+import About from './About'
+import FAQ from './FAQ'
+import Contact from './Contact'
 
 export default function App() {
   return (
@@ -12,10 +11,9 @@ export default function App() {
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* Example routes: */}
-          {/* <Route path="/about" element={<About />} /> */}
-          {/* <Route path="/faq" element={<FAQ />} /> */}
-          {/* <Route path="/contact" element={<Contact />} /> */}
+          <Route path="/about" element={<About />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Layout>
     </Router>
