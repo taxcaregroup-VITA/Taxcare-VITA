31d690ea8e05f3c68b69a43948f0b29a9fb7cb71
taxcaregroup-VITA <taxcaregroupinc24@gmail.com>
2026-01-06 16:58:09 -0500

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,15 @@
 import HeroSection from './components/home/HeroSection'
 import EligibilitySection from './components/home/EligibilitySection'
 import TestimonialsSection from './components/home/TestimonialsSection'
+import SignUpSection from './components/home/SignUpSection'
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <EligibilitySection />
-      <TestimonialsSection />
+      TestimonialsSection />
+      <SignUpSection />
     </>
   )
 }
