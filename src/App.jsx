7f933e07cf35c16cc03ad2b18bd95def263c6c17
taxcaregroup-VITA <taxcaregroupinc24@gmail.com>
2026-01-06 19:09:01 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Layout from './Layout'
 import Home from './Home'
 import About from './About'
 import FAQ from './FAQ'
 import Contact from './Contact'
+import Privacy from './Privacy';
+import GetStarted from './GetStarted';
+import Schedule from './Schedule';
+
 
 export default function App() {
   return (
@@ -14,6 +19,10 @@ export default function App() {
           <Route path="/about" element={<About />} />
           <Route path="/faq" element={<FAQ />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/privacy" element={<Privacy />} />
+<Route path="/get-started" element={<GetStarted />} />
+<Route path="/schedule" element={<Schedule />} />
+
         </Routes>
       </Layout>
     </Router>
