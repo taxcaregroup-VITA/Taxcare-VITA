3e0c3a1f7b38c1a5823662adf8bb1e721ffd260d
taxcaregroup-VITA <taxcaregroupinc24@gmail.com>
2026-01-15 01:09:30 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
-import Layout from './Layout'
-import Home from './Home'
-import GetStarted from './GetStarted'
-import Schedule from './Schedule'
-import FAQ from './FAQ'
-import About from './About'
-import Privacy from './Privacy'
-import Contact from './Contact'
-import { Routes, Route } from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './Layout';
+import Home from './Home';
+import About from './About';
+import FAQ from './FAQ';
+import Contact from './Contact';
+import Privacy from './Privacy';
+import GetStarted from './GetStarted';
+import Schedule from './Schedule';
 
 export default function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Layout>
-  )
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/get-started" element={<GetStarted />} />
+          <Route path="/schedule" element={<Schedule />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
 }
