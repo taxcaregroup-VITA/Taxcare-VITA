488a8d4d35b9993bf91df92db3923a76cbe9e6c7
taxcaregroup-VITA <taxcaregroupinc24@gmail.com>
2026-01-15 00:45:17 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './Layout';
-
-// Placeholder pages
-function Home() { return <div className="p-10 text-center">Welcome to Tax Care VITA!</div>; }
-function About() { return <div className="p-10 text-center">About Page</div>; }
-function FAQ() { return <div className="p-10 text-center">FAQ Page</div>; }
-function Contact() { return <div className="p-10 text-center">Contact Page</div>; }
-function Privacy() { return <div className="p-10 text-center">Privacy Policy Page</div>; }
-function GetStarted() { return <div className="p-10 text-center">Get Started Page</div>; }
-function Schedule() { return <div className="p-10 text-center">Schedule Appointment Page</div>; }
+import Layout from './Layout'
+import Home from './Home'
+import { Routes, Route } from 'react-router-dom'
 
 export default function App() {
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/faq" element={<FAQ />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/privacy" element={<Privacy />} />
-          <Route path="/get-started" element={<GetStarted />} />
-          <Route path="/schedule" element={<Schedule />} />
-        </Routes>
-      </Layout>
-    </Router>
-  );
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Layout>
+  )
 }
