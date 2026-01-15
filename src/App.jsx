8926c6de12ca9bf108c3a0e94efc28226556dbd9
taxcaregroup-VITA <taxcaregroupinc24@gmail.com>
2026-01-15 01:01:12 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import Layout from './Layout'
 import Home from './Home'
+import Home from './GetStarted'
+import Home from './Schedule'
+import Home from './FAQ'
+import Home from './About'
+import Home from './Privacy'
+import Home from './Contact'
 import { Routes, Route } from 'react-router-dom'
 
 export default function App() {
