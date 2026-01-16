763022351a14c16f2aeec63d70639e57ec9020e2
taxcaregroup-VITA <taxcaregroupinc24@gmail.com>
2026-01-16 18:47:54 -0500

    Update EligibilitySection.jsx

--- src/components/home/EligibilitySection.jsx
+++ src/components/home/EligibilitySection.jsx
@@ -10,9 +10,7 @@ export default function EligibilitySection() {
 
   return (
     <section className="flex items-start gap-3
-  bg-gray-50 p-4 rounded-xl
-  shadow-sm hover:shadow-lg hover:-translate-y-1
-  transition-all duration-300">
+  bg-gray-50 p-4 rounded-xl">
       <h2 className="text-2xl font-bold mb-4">Who Qualifies?</h2>
       <p className="text-gray-600 max-w-2xl mx-auto mb-6">
         VITA services are available to individuals and families who meet the following criteria:
