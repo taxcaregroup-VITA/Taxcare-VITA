06ba5547efb872ae0034e510ed26408213d07d7b
taxcaregroup-VITA <taxcaregroupinc24@gmail.com>
2026-01-16 18:57:07 -0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,17 @@
-import HeroSection from '../components/home/HeroSection'
-import BenefitsSection from '../components/home/BenefitsSection'
-import EligibilitySection from '../components/home/EligibilitySection'
-import TestimonialsSection from '../components/home/TestimonialsSection'
-import SignUpSection from '../components/home/SignUpSection'
+import HeroSection from "../components/home/HeroSection";
+import BenefitsSection from "../components/home/BenefitsSection";
+import EligibilitySection from "../components/home/EligibilitySection";
+import TestimonialsSection from "../components/home/TestimonialsSection";
+import SignUpSection from "../components/home/SignUpSection";
 
 export default function Home() {
   return (
-    <div className="space-y-24">
-  <HeroSection />
-  <BenefitsSection />
-  <EligibilitySection />
-  <TestimonialsSection />
-  <SignUpSection />
-</div>
-
-  )
+    <>
+      <HeroSection />
+      <BenefitsSection />
+      <EligibilitySection />
+      <TestimonialsSection />
+      <SignUpSection />
+    </>
+  );
 }
