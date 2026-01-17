b6635a004bca30f187568f78dcc5c71afd2524e3
taxcaregroup-VITA <taxcaregroupinc24@gmail.com>
2026-01-16 23:23:54 -0500

    Update OtherTaxesStep.jsx

--- src/components/intake/OtherTaxesStep.jsx
+++ src/components/intake/OtherTaxesStep.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { Input } from "../components/ui/input";
+import { Label } from "../components/ui/label";
 import { useLanguage } from '../LanguageContext';
 
 export default function OtherTaxesStep({ data, onChange }) {
