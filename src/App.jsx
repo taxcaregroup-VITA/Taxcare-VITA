2c3f68e7dc8fee49537bd0a9bf0f35e8c0806c2a
taxcaregroup-VITA <taxcaregroupinc24@gmail.com>
2026-01-17 05:52:54 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,5 @@
-import { Routes, Route } from "react-router-dom";
-
 import Home from "./pages/Home";
-import GetStarted from "./pages/GetStarted";
-import IntakeForm from "./components/intake/IntakeForm";
 
 export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/get-started" element={<GetStarted />} />
-      <Route path="/intake" element={<IntakeForm />} />
-    </Routes>
-  );
+  return <Home />;
 }
