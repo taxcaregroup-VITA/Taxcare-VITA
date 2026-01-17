983326d878e15b32958d9f2d57937457e3f59d11
taxcaregroup-VITA <taxcaregroupinc24@gmail.com>
2026-01-16 23:21:57 -0500

    Update HealthInsuranceStep.jsx

--- src/components/intake/HealthInsuranceStep.jsx
+++ src/components/intake/HealthInsuranceStep.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
-import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "../components/ui/radio-group";
+import { Label } from "../components/ui/label";
 import { useLanguage } from '../LanguageContext';
 
 export default function HealthInsuranceStep({ data, onChange }) {
