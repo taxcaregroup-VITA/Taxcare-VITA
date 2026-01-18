c7ab848839f4d56810663354e019cc3024152689
taxcaregroup-VITA <taxcaregroupinc24@gmail.com>
2026-01-18 01:30:59 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
+import Home from "./pages/Home";
+
 export default function App() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>App is rendering</h1>
-    </div>
-  );
+  return <Home />;
 }
