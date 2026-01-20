38ac023034b102ad1ca78afd02d22e27dc26a56b
taxcaregroup-VITA <taxcaregroupinc24@gmail.com>
2026-01-19 21:08:17 -0500

    Create PartVI_Notes.jsx

---
+++ src/pdf/PartVI_Notes.jsx
@@ -0,0 +1,18 @@
+import { View, Text } from "@react-pdf/renderer";
+import styles from "./styles";
+
+export default function PartVI_Notes({ data }) {
+  if (!data) return null;
+
+  return (
+    <View style={styles.section}>
+      <Text style={styles.header}>
+        Part VI – Notes / Comments
+      </Text>
+
+      <View style={styles.notesBox}>
+        <Text>{data}</Text>
+      </View>
+    </View>
+  );
+}
