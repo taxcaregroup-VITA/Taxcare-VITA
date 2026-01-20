151f371324a6e8cffc7ff31f469993d82d86ac9f
taxcaregroup-VITA <taxcaregroupinc24@gmail.com>
2026-01-20 17:09:37 -0500

    Update Review.jsx

--- src/pages/Review.jsx
+++ src/pages/Review.jsx
@@ -1,5 +1,5 @@
 import { useLocation } from "react-router-dom";
-import { generatePDFs } from "../pdf/generatePdf";
+import { generatePDF } from "../pdf/generatePDF";
 
 export default function Review() {
   const { state: formData } = useLocation();
